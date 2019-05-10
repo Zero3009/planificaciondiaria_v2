48c767c9c988045c1b21292fb5f4e89130605e7f
Franco <fmaggioni0@gmail.com>
2019-05-10 10:34:17 -0300

    login bug

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,53 @@
-@extends('layouts.welcome')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
+    <link rel="shortcut icon" href="/img/favicon.ico">
+    <title>Planificación</title>
 
-<div class="container">
+    <link rel="stylesheet" href="/plugins/bootstrap-3.3.7/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="/estilos.css" />
+    <link rel="stylesheet" href="/welcome.css" />
+   
+</head>
+<body id="app-layout">  
+    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container-fluid">
+            <div class="navbar-left">
+                <a class="navbar-brand" href="{{ route('welcome') }}"><img src="/img/logo.png" alt="Dispute Bills"></a>
+            </div>
+            <div class="navbar-right">
+                <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                        <!-- Right Side Of Navbar -->
+                        <ul class="nav navbar-nav navbar-right" style="width: 100%;">
+                            <!-- Authentication Links -->
+                            @if (Auth::guest())
+                                <li class="dropdown" style="float: right;margin-right: 20px;">
+                                    <a href="{{ route('login') }}" style="color: white;"><i class="glyphicon glyphicon-log-in"></i> Iniciar sesión</a>
+                                </li>
+                            @else
+                                <li class="dropdown" style="float: right;margin-right: 20px;">
+                                    <a class="dropdown-item" style="color: white;" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        <i class="glyphicon glyphicon-log-out"></i> Cerrar sesión</a>
+                                    </a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form> 
+                                </li>
+                            @endif
+
+                        </ul>          
+                </div>
+            </div>  
+        </div>
+    </div>
+
+    <div id="container">
     <div class="row" style="margin-top: 20px;">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
@@ -48,4 +93,32 @@
         </div>
     </div>
 </div>
-@endsection
+<script src="/plugins/jQuery/jquery-3.2.1.min.js"></script>
+<script src="/plugins/bootstrap-3.3.7/js/bootstrap.min.js"></script>
+<script type="text/javascript">
+$(document).ready( function () {
+    $('#pagina-1').click(function() {
+        window.location.href = "{{route('planificacion')}}" ;
+        return false;
+    });
+    $('#pagina-2').click(function() {
+        window.location.href = "{{route('visualizador')}}";
+        return false;
+    });
+    $('#pagina-3').click(function() {
+        window.location.href = "{{route('dashboard')}}" ;
+        return false;
+    });
+    $('#pagina-4').click(function() {
+        window.location.href = "{{route('serviciosua')}}" ;
+        return false;
+    });
+    $('#pagina-5').click(function() {
+        window.location.href = "{{route('formproblemas')}}" ;
+        return false;
+    });
+});
+</script>
+</body>
+</html>
+
